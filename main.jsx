6e24b9f88b577fec464d05c46750067393c22309
Erikka A. <114741859+Akkirea@users.noreply.github.com>
2026-04-06 07:59:32 +0800

    Convert app to Vite React

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TrackerDashboard from "./Tracker-dashboard.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TrackerDashboard />
+  </React.StrictMode>
+);
